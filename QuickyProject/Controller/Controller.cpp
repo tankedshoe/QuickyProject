115f320bd0e22b060eed7b84210c5965fd55fc60
tankedshoe <tankedshoe@gmail.com>
2018-01-25 08:51:04 -0700

    Made a test loop.

--- QuickyProject/Controller/Controller.cpp
+++ QuickyProject/Controller/Controller.cpp
@@ -7,3 +7,19 @@
 //
 
 #include "Controller.hpp"
+
+using namespace std;
+
+Controller :: Controller()
+{
+    
+}
+
+void Controller:: start()
+{
+    for (int index = 0; index < 10; index++)
+    {
+        cout <<"This loop has executed " << index + 1 << " times" << endl;
+    }
+}
+
